0a6da736252f3135fbe246a761dce7d2da21d2f1
visay vang <vsvang@gmail.com>
2023-04-29 14:20:40 -0400

    added google maps, added geolocation

--- static/js/meet_up.js
+++ static/js/meet_up.js
@@ -1,13 +1,52 @@
+const cityInput = document.getElementById("city");
+const stateInput = document.getElementById("state");
+const searchBtn = document.getElementById("search-button");
+
+// Geolocation
+const findMyState = () => {
+ 
+  const success = (position) => {
+    let cityEl = cityInput.value;
+    let stateEl = stateInput.value;
+
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+    fetch(geoApiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        
+        // findCity.textContent = data.city;
+        // status.textContent = data.principalSubdivision;
+    })
+  }
+  const error = () => {
+    console.log("Error, unable to retrieve data")
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+
+}
+searchBtn.addEventListener("click", findMyState);
+
 
 // google maps
 function initMap() {
+  
   let options = {
-    lat: 34.8726,
-    lng: 82.3625
+    lat: 34.5034,
+    lng: -82.6501
   }
-  const basicMap = new google.maps.Map(document.getElementById("map"), {
+  let map = new google.maps.Map(document.getElementById("map"), {
     center: options,
-    zoom: 10
+    zoom: 13,
+  });
+
+  let marker = new google.maps.Marker({
+    position: options,
+    map: map
   })
 
 }
