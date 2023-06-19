d8923b2f5b313d833ed590b244f0a0ed24adee89
visay vang <vsvang@gmail.com>
2023-06-19 10:51:06 -0400

    change layout, font-size

--- static/js/meet_up.js
+++ static/js/meet_up.js
@@ -7,7 +7,7 @@ function initMap() {
  
   let map = new google.maps.Map(document.getElementById("map"), {
     center: options,
-    zoom: 10,
+    zoom: 12,
   });
 
   let geocoder = new google.maps.Geocoder()
